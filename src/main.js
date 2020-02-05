95f13f2f2e118e14b30732bbed5a7189f417fd53
Krista Rutz and neha doodi <krista@>
2020-02-04 16:52:48 -0800

    add a displayPuzzle function

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@ import $ from 'jquery';
 import 'bootstrap' ; 
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
+import { Puzzle } from './sudoku';
+
+function displayPuzzle(puzzle){
+  var htmlOutput = "Rows, 1 2 3 4 5 6 7 8 9 <br>";
+  for(var i = 0; i<9; i++){
+    htmlOutput += "Row  :" + i+1;
+    for (var j=0; j<9; j++){
+      htmlOutput += j + " ";
+    } 
+    htmlOutput += "<br>";
+  }
+  $("body").html(htmlOutput);
+}
 
 $(document).ready(function() {
-
+  var puzzle = new Puzzle();
+  puzzle.autofillPuzzle();
+  displayPuzzle(puzzle);
 });
-
-
-// sudokuStr = "1 2 3 4 5 6 7 8 9 2 3 4 5 6 7 8 9 1 3 "
-// sudokuArr = [1, 2, 3, 4 ]
-// sudokuObj = { 
-//   row1 = [1,2,3,4,5,6,7,8,9];
-//   row2 = [4,5,6,7,8,9,1,2,3];
-// } 
